6cf9711b86950ffd57fa6ff05ef0976bb6d91590
Faruk khan <fk300220@gmail.com>
2022-11-09 12:40:30 +0530

    major functionalities working

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,55 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [current,setCurrent]=useState("")
+  const evaluation=()=>{
+    if(typeof(current)=='string'){
+      setCurrent(current.slice(0,current.length-1))
+    }
+    else{
+      // console.log(current.toString().slice(0,current.length-1))
+      const str=current.toString()
+      setCurrent(str.slice(0,str.length-1))
+    }
+  }
   return (
     <div className="App">
       {/* <h1>Calculator</h1> */}
       <div className='mainContainer'>
         <div className='screen'>
-
+          <h1>{current}</h1>
         </div>
         <div className='keySection'>
           <div>
-            <button>AC</button>
+            <button onClick={()=>setCurrent("")}>AC</button>
             <button>%</button>
-            <button>C</button>
-            <button>÷</button>
-
+            <button onClick={()=>evaluation()}>C</button>
+            <button onClick={()=>setCurrent(current+"/")}>÷</button>
           </div>
           <div>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>X</button>
+            <button onClick={()=>setCurrent(current+"1")}>1</button>
+            <button onClick={()=>setCurrent(current+"2")}>2</button>
+            <button onClick={()=>setCurrent(current+"3")}>3</button>
+            <button onClick={()=>setCurrent(current+"*")}>X</button>
           </div>
           <div>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>-</button>
+            <button onClick={()=>setCurrent(current+"4")}>4</button>
+            <button onClick={()=>setCurrent(current+"5")}>5</button>
+            <button onClick={()=>setCurrent(current+"6")}>6</button>
+            <button onClick={()=>setCurrent(current+"-")}>-</button>
           </div>
           <div>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>+</button>
+            <button onClick={()=>setCurrent(current+"7")}>7</button>
+            <button onClick={()=>setCurrent(current+"8")}>8</button>
+            <button onClick={()=>setCurrent(current+"9")}>9</button>
+            <button onClick={()=>setCurrent(current+"+")}>+</button>
           </div>
           <div>
-            <button>.</button>
-            <button>0</button>
-            <button>00</button>
-            <button>=</button>
+            <button onClick={()=>setCurrent(current+".")}>.</button>
+            <button onClick={()=>setCurrent(current+"0")}>0</button>
+            <button onClick={()=>setCurrent(current+"00")}>00</button>
+            <button onClick={()=>setCurrent(eval(current))}>=</button>
           </div>
         </div>
       </div>
