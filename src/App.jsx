dd6fc671c2fea021322c699faac8409654b5920c
Faruk khan <fk300220@gmail.com>
2022-11-09 13:02:50 +0530

    minor ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
   }
   return (
     <div className="App">
+      <h2 id='title'>Calculator</h2>
       {/* <h1>Calculator</h1> */}
       <div className='mainContainer'>
         <div className='screen'>
@@ -22,34 +23,34 @@ function App() {
         </div>
         <div className='keySection'>
           <div>
-            <button onClick={()=>setCurrent("")}>AC</button>
-            <button>%</button>
-            <button onClick={()=>evaluation()}>C</button>
-            <button onClick={()=>setCurrent(current+"/")}>÷</button>
+            <button id='allClearbtn' onClick={()=>setCurrent("")}>AC</button>
+            <button id='actionBtns'>%</button>
+            <button id='deletebtn' onClick={()=>evaluation()}>C</button>
+            <button id='actionBtns' onClick={()=>setCurrent(current+"/")}>÷</button>
           </div>
           <div>
             <button onClick={()=>setCurrent(current+"1")}>1</button>
             <button onClick={()=>setCurrent(current+"2")}>2</button>
             <button onClick={()=>setCurrent(current+"3")}>3</button>
-            <button onClick={()=>setCurrent(current+"*")}>X</button>
+            <button id='actionBtns' onClick={()=>setCurrent(current+"*")}>X</button>
           </div>
           <div>
             <button onClick={()=>setCurrent(current+"4")}>4</button>
             <button onClick={()=>setCurrent(current+"5")}>5</button>
             <button onClick={()=>setCurrent(current+"6")}>6</button>
-            <button onClick={()=>setCurrent(current+"-")}>-</button>
+            <button id='actionBtns' onClick={()=>setCurrent(current+"-")}>-</button>
           </div>
           <div>
             <button onClick={()=>setCurrent(current+"7")}>7</button>
             <button onClick={()=>setCurrent(current+"8")}>8</button>
             <button onClick={()=>setCurrent(current+"9")}>9</button>
-            <button onClick={()=>setCurrent(current+"+")}>+</button>
+            <button id='actionBtns' onClick={()=>setCurrent(current+"+")}>+</button>
           </div>
           <div>
             <button onClick={()=>setCurrent(current+".")}>.</button>
             <button onClick={()=>setCurrent(current+"0")}>0</button>
             <button onClick={()=>setCurrent(current+"00")}>00</button>
-            <button onClick={()=>setCurrent(eval(current))}>=</button>
+            <button id='submitbtn' onClick={()=>setCurrent(eval(current))}>=</button>
           </div>
         </div>
       </div>
